1ebc5974dce93433abbe0cd9f7c18c19f9ba68bb
tomisin <tomjames156@gmail.com>
2023-12-02 15:43:21 +0100

    added escape value for while loop

--- hash.py
+++ hash.py
@@ -7,8 +7,11 @@ salt = bcrypt.gensalt() # generate salt for the password
 hashed_pass = bcrypt.hashpw(password, salt) # get hash for the password using the salt
 
 while not(correct_pass):
-    # Keeps asking the user until they enter the correct password
-    user_pass = input("\nEnter your password: ") # allow user to enter their password
+    # Keeps asking the user until they enter the correct password or the escape value
+    user_pass = input("\nEnter your password or 'q' to Quit: ") # allow user to enter their password
+    if(user_pass == 'q'): # break out of the loop
+        print("Goodbye!")
+        break
     user_pass = str.encode(user_pass) # convert the password to a byte string
 
     if bcrypt.checkpw(user_pass, hashed_pass): # if password entered is correct
