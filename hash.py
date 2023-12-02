282956fb933fe7f528cb9ac49fb6f9431b023f78
tomisin <tomjames156@gmail.com>
2023-12-02 15:34:27 +0100

    added code comments

--- hash.py
+++ hash.py
@@ -1,17 +1,18 @@
 import bcrypt
 
-correct_pass = False
-user_pass = b''
-password = b'supersecretpassword'
-salt = bcrypt.gensalt()
-hashed_pass = bcrypt.hashpw(password, salt)
+correct_pass = False # boolean to check whether password is correct or not
+user_pass = b'' 
+password = b'supersecretpassword'  # user stored plain text password
+salt = bcrypt.gensalt() # generate salt for the password
+hashed_pass = bcrypt.hashpw(password, salt) # get hash for the password using the salt
 
 while not(correct_pass):
-    user_pass = input("\nEnter your password: ")
-    user_pass = str.encode(user_pass)
+    # Keeps asking the user until they enter the correct password
+    user_pass = input("\nEnter your password: ") # allow user to enter their password
+    user_pass = str.encode(user_pass) # convert the password to a byte string
 
-    if bcrypt.checkpw(user_pass, hashed_pass):
-        correct_pass = True
+    if bcrypt.checkpw(user_pass, hashed_pass): # if password entered is correct
+        correct_pass = True # boolean to check password set to True and stop asking for password
         print("\nAccess Granted, Welcome back Tomi")
-    else:
+    else: # if password entered is not correct
         print("\nSorry, Wrong Password")
